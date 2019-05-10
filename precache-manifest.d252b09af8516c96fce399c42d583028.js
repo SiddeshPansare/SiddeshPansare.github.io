23123e60b5600b872c6ee1985716693e0a79a37b
SiddeshPansare <40264381+SiddeshPansare@users.noreply.github.com>
2019-05-10 17:55:10 +0530

    added for testing

---
+++ precache-manifest.d252b09af8516c96fce399c42d583028.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c08fd9bf02d1e20ec4cf5ede31664cff",
+    "url": "/static/media/userAssistance.c08fd9bf.svg"
+  },
+  {
+    "revision": "cd3abf7e8df2985e09a7",
+    "url": "/static/css/main.ef024607.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e8b31fe85ed16414cfb0f6799fe6f106",
+    "url": "/static/media/banner3.e8b31fe8.png"
+  },
+  {
+    "revision": "6e0878001fcc755208c0",
+    "url": "/static/js/2.6e7c3a19.chunk.js"
+  },
+  {
+    "revision": "26847b995bbf99490c0e0539fe0590db",
+    "url": "/static/media/singlePoint.26847b99.svg"
+  },
+  {
+    "revision": "cd3abf7e8df2985e09a7",
+    "url": "/static/js/main.8f6a5b68.chunk.js"
+  },
+  {
+    "revision": "765267fa2431c78f86498b6e0a18b139",
+    "url": "/static/media/userConvinience.765267fa.svg"
+  },
+  {
+    "revision": "256c0a90336c076b13d6f43e00bc3147",
+    "url": "/static/media/compliance.256c0a90.svg"
+  },
+  {
+    "revision": "fab4517ac604899cf1baa76383487bb5",
+    "url": "/static/media/banner1.fab4517a.png"
+  },
+  {
+    "revision": "6e0878001fcc755208c0",
+    "url": "/static/css/2.b26a74bb.chunk.css"
+  },
+  {
+    "revision": "ca8fb1b494ad501b597e50457e8aa7aa",
+    "url": "/index.html"
+  }
+];
